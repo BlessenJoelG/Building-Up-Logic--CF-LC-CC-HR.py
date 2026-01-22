60eea2c6453470095576cda20eefc5018959396f
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-22 10:52:57 +0530

    InterviewBite

--- Dry_Run/dryrun.py
+++ Dry_Run/dryrun.py
@@ -1,1 +1,9 @@
-pass
+def main():
+    my_string = "InterviewBit"
+    no_space = ""
+    for i in range (101):
+        res = no_space + my_string
+    return res
+
+if __name__ == '__main__':
+    main()
