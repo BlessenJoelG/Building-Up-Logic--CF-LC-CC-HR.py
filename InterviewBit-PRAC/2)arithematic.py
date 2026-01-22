928730e2e0686eb270ab49bae042b8510c43a701
Blessen Joel Gorinkala <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-22 10:55:11 +0530

    Add basic variable declarations and print statements

---
+++ InterviewBit-PRAC/2)arithematic.py
@@ -0,0 +1,8 @@
+my_string1 = "InterviewBit"
+my_string2 = "Dont't change the variable name"
+my_int = 11
+my_float = 20.0
+print(" String1: %s" % my_string1)
+print(" String2: %s" % my_string2)
+print(" Integer: %d" % my_int)
+print(" Float: %f" % my_float)
