53eece439db8d7a44d153fc865bd65aa40899cb8
Blessen Joel Gorinkala <g.blessenjoel@gmail.com>
2026-02-14 14:24:49 +0530

    Fix missing newline at end of file

--- CODEFORCES-PRAC/228A Is your horseshoe on the other hoof.py
+++ CODEFORCES-PRAC/228A Is your horseshoe on the other hoof.py
@@ -1,4 +1,4 @@
 have_to_buy_shoes = 4
 had_shoes = set()
 had_shoes.update(map(int,input().rstrip().split()))
-print((have_to_buy_shoes)-(len(had_shoes))
+print((have_to_buy_shoes)-(len(had_shoes)))
